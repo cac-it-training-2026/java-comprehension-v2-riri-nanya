a6ff2f33384a87980d76a8d41d901523d5b9141b
ririkana <ririkana@C010423486.zipangu.cac.co.jp>
2026-05-01 13:54:09 +0900

    FIX 0501 q02_advance.question01まで完了
    理解できた点
    ・継承の仕方
    
    不明が残った点
    ・拡張for文の使い方

---
+++ java_comprehension_exercises_volume2_NO_NAME/src/q02_advanced/question01/Member.java
@@ -0,0 +1,125 @@
+package q02_advanced.question01;
+
+import java.util.List;
+
+public class Member {
+
+	/**
+	 * 
+	 */
+	public Member() {
+
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	private int id;
+	private String password;
+	private String name;
+	private int age;
+	private int rank;
+	private List<Coupon> coupons;
+
+	/**
+	 * @return id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id セットする id
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password セットする password
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name セットする name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+	/**
+	 * @param age セットする age
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	/**
+	 * @return rank
+	 */
+	public int getRank() {
+		return rank;
+	}
+
+	/**
+	 * @param rank セットする rank
+	 */
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+
+	/**
+	 * @return coupons
+	 */
+	public List<Coupon> getCoupons() {
+		return coupons;
+	}
+
+	/**
+	 * @param coupons セットする coupons
+	 */
+	public void setCoupons(List<Coupon> coupons) {
+		this.coupons = coupons;
+	}
+
+	public Member(int id, String password, String name, int age, int rank) {
+		this.id = id;
+		this.password = password;
+		this.name = name;
+		this.age = age;
+		this.rank = rank;
+
+	}
+
+	public void showMember() {
+		System.out.println("id=" + id);
+		System.out.println("password=" + password);
+		System.out.println("name=" + name);
+		System.out.println("age=" + age);
+		System.err.println("rank=" + rank);
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
